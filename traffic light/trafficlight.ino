befe519e0d957a0d15014ddeb35ddece74e81ecb
Subramanian <subrueng26@gmail.com>
2026-05-14 13:59:57 +0530

    Change yellow light delay to 2000 milliseconds
    Adjusted delay for the yellow light to 2000 milliseconds.

--- traffic light/trafficlight.ino
+++ traffic light/trafficlight.ino
@@ -12,10 +12,18 @@ void loop()
   digitalWrite(13, HIGH);
   delay(5000); // Wait for 1000 millisecond(s)
   digitalWrite(13, LOW);
+  
   digitalWrite(12, HIGH);
-  delay(5000); // Wait for 1000 millisecond(s)
+  delay(2000); // Wait for 1000 millisecond(s)
   digitalWrite(12, LOW);
+  
   digitalWrite(11, HIGH);
   delay(5000); // Wait for 1000 millisecond(s)
   digitalWrite(11, LOW);
+
+  digitalWrite(12, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
+  digitalWrite(12, LOW);
+  
+  
 }
